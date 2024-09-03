70461683f4d19d045b03104813ebfd5f2ce19483
Filming <79931565+filming@users.noreply.github.com>
2024-09-03 18:48:53 -0400

    refactor: Make getting user data process more modular

--- src/cogs/events/on_message.py
+++ src/cogs/events/on_message.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 
 import logging
+import sqlite3
+from typing import Dict
 
 logger = logging.getLogger("discord")
 
@@ -12,21 +14,25 @@ class OnMessage(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
 
-    @commands.Cog.listener()
-    async def on_message(self, message: discord.Message) -> None:
-        """Process messages that are received through the bot."""
+    def get_user_data(self, message: discord.Message) -> Dict[str, int]:
+        """Return a dict representing the level table data of a user."""
 
-        # Only respond to messages from guilds
-        if message.channel.type != "private":
-            cur = self.bot.db.cursor()
+        user_data = {
+            "user_id": message.author.id,
+            "guild_id": message.guild.id,
+        }
 
-            # Check if user exists, add if not
+        cur = self.bot.db.cursor()
+
+        # Check if user exists in db, add them if not
+        try:
             res = cur.execute(
                 "SELECT * from level WHERE user_id = ? AND guild_id = ?",
                 (message.author.id, message.guild.id),
             ).fetchone()
 
-            if res == None:
+            if not res:
+                # Add user to level table
                 cur.execute(
                     "INSERT INTO level (user_id, guild_id) VALUES (?, ?)",
                     (message.author.id, message.guild.id),
@@ -39,7 +45,33 @@ class OnMessage(commands.Cog):
                     message.guild.id,
                 )
 
+                user_data["experience"] = 0
+                user_data["level"] = 0
+                user_data["previous_message_timestamp"] = 0
+
+            else:
+                user_data["experience"] = res[2]
+                user_data["level"] = res[3]
+                user_data["previous_message_timestamp"] = res[4]
+
             cur.close()
+
+        except sqlite3.Error as e:
+            logger.error(f"Error fetching/adding user data: {e}")
+
+        return user_data
+
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message) -> None:
+        """Process messages that are received through the bot."""
+
+        # Only respond to messages from guilds
+        if message.channel.type != "private":
+
+            user_data = self.get_user_data(message)
+
+
+# TODO: Implement XP gain, level-up logic, cooldown/rate limiting, etc.
 
 
 async def setup(bot: commands.Bot) -> None:
