7c9f7f81b5bbc1021237e0ede0dc4f7ea6f09eee
Filming <79931565+filming@users.noreply.github.com>
2024-09-03 17:52:52 -0400

    feat: Add new user to level table

--- src/cogs/events/on_message.py
+++ src/cogs/events/on_message.py
@@ -7,16 +7,40 @@ logger = logging.getLogger("discord")
 
 
 class OnMessage(commands.Cog):
-	"""Cog for handling message events."""
+    """Cog for handling message events."""
 
-	def __init__(self, bot: commands.Bot) -> None:
-		self.bot = bot
+    def __init__(self, bot: commands.Bot) -> None:
+        self.bot = bot
 
-	@commands.Cog.listener()
-	async def on_message(self, message: discord.Message) -> None:
-		"""Process messages that are received through the bot."""
-		pass
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message) -> None:
+        """Process messages that are received through the bot."""
+
+        # Only respond to messages that are from guilds
+        if message.channel.type != "private":
+            cur = self.bot.db.cursor()
+
+            # Add user to DB if they aren't already in it
+            res = cur.execute(
+                "SELECT * from level WHERE user_id = ? AND guild_id = ?",
+                (message.author.id, message.guild.id),
+            ).fetchone()
+
+            if res == None:
+                cur.execute(
+                    "INSERT INTO level (user_id, guild_id) VALUES (?, ?)",
+                    (message.author.id, message.guild.id),
+                )
+                self.bot.db.commit()
+
+                logger.info(
+                    "New user added to the level table. (userId: %s, guildId: %s)",
+                    message.author.id,
+                    message.guild.id,
+                )
+
+            cur.close()
 
 
 async def setup(bot: commands.Bot) -> None:
-	await bot.add_cog(OnMessage(bot))
+    await bot.add_cog(OnMessage(bot))
